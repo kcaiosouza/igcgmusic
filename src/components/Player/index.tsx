a8e0c14fcd93dc1a0e431b4e6a12f9b68035a259
Caio <kcaiosouza@gmail.com>
2024-11-14 16:10:43 -0300

    :sparkles: feat: player component update

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -6,8 +6,8 @@ import Slider from 'rc-slider';
 import {
   IoHeartOutline,
   IoHeart,
-  IoPlay,
-  IoPause,
+  IoPlayCircle,
+  IoPauseCircle,
   IoPlaySkipBack,
   IoPlaySkipForward,
   IoShuffle,
@@ -25,9 +25,9 @@ export default function Player() {
   const t = useTranslations("PlayerComponent");
   return(
     <section className="flex flex-col w-[324px] max-w-[324px] bg-[#2E2E20] justify-center">
-      <section className="pl-4 pt-3 flex flex-col gap-2">
-        <h3 className="font-semibold text-xl">Fila de Músicas</h3>
-        <div className="w-full h-auto overflow-scroll gap-1 flex flex-row">
+      <section className="pt-3 flex flex-col gap-2">
+        <h3 className="font-semibold text-xl pl-4">Fila de Músicas</h3>
+        <div className="pl-4 w-full h-auto overflow-scroll gap-1 flex flex-row">
           <CardMusic/>
           <CardMusic/>
           <CardMusic/>
@@ -37,28 +37,30 @@ export default function Player() {
         </div>
       </section>
       <section className="w-full flex flex-[1] flex-col items-center justify-center">
-        <div className="bg-[#5B6143] w-[80%] h-[346px] rounded-3xl flex flex-col items-center justify-between p-6">
+        <div className="bg-[#5B6143] w-[80%] h-[346px] rounded-3xl flex flex-col items-center justify-between p-6 gap-2">
           <div className="flex flex-col items-center">
             <Image src="https://github.com/kcaiosouza.png" alt="Capa CD" width={164} height={164} className="rounded-2xl mb-2"/>
             <span>Nome da Música</span>
             <span>Nome do Artista</span>
           </div>
-          <div className="flex flex-row items-center justify-center gap-1">
-            <IoRepeat size={23}/>
-            <IoPlaySkipBack size={23}/>
-            <IoPlay size={32} className="translate-x-[1px]"/>
-            <IoPlaySkipForward size={23}/>
-            <IoShuffle size={23}/>
-          </div>
-          <div className="flex flex-row w-full px-14 items-center justify-center gap-2">
-            <IoVolumeHigh size={23}/>
-            <Slider className="volume-slider"/>
+          <div className="flex flex-col w-full">
+            <div className="flex flex-row items-center justify-center gap-1">
+              <IoRepeat size={23}/>
+              <IoPlaySkipBack size={23}/>
+              <IoPlayCircle size={54} className="translate-x-[1px]"/>
+              <IoPlaySkipForward size={23}/>
+              <IoShuffle size={23}/>
+            </div>
+            <div className="flex flex-row w-full px-12 items-center justify-center gap-2">
+              <IoVolumeHigh size={23}/>
+              <Slider className="volume-slider"/>
+            </div>
           </div>
         </div>
         <div className="flex flex-row items-center justify-center gap-2 px-2 bg-[#5B6143] w-[64%] h-11 rounded-b-2xl">
-          <span>01:15</span>
+          <span className="text-[12px]">01:15</span>
           <Slider className="music-slider"/>
-          <span>15:15</span>
+          <span className="text-[12px]">15:15</span>
         </div>
       </section>
     </section>
